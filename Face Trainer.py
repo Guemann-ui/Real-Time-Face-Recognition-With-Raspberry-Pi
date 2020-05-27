61a89616ddda0e1d9a2708ce801bbc7e2aa6cbb6
Besma Guesmi <36562278+besmaGuesmi@users.noreply.github.com>
2020-05-27 03:44:51 +0200

    Update Face Trainer.py

--- Face Trainer.py
+++ Face Trainer.py
@@ -1,11 +1,9 @@
-#Program to train with the faces and create a YAML file
-
 import cv2 #For Image processing 
-import numpy as np #For converting Images to Numerical array 
-import os #To handle directories 
+import numpy as np #convert Images to Numerical array 
+import os #To handle directories and provides functions for interacting with the operating system
 from PIL import Image #Pillow lib for handling images 
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') #Cascade classifier class for object detection / XML pre-trained classifiers for face
 recognizer = cv2.createLBPHFaceRecognizer()
 
 Face_ID = -1 
@@ -13,8 +11,8 @@ pev_person_name = ""
 y_ID = []
 x_train = []
 
-Face_Images = os.path.join(os.getcwd(), "Face_Images") #Tell the program where we have saved the face images 
-print (Face_Images)
+Face_Images = os.path.join(os.getcwd(), "Face Images") #path of your "Face Image" Directory
+print (Face Images)
 
 for root, dirs, files in os.walk(Face_Images): #go to the face image directory 
 	for file in files: #check every directory in it 
@@ -30,14 +28,16 @@ for root, dirs, files in os.walk(Face_Images): #go to the face image directory
 
 			
 			Gery_Image = Image.open(path).convert("L") # convert the image to greysclae using Pillow
-			Crop_Image = Gery_Image.resize( (550,550) , Image.ANTIALIAS) #Crop the Grey Image to 550*550 (Make sure your face is in the center in all image)
+			Crop_Image = Gery_Image.resize( (550,550) , Image.ANTIALIAS) #resize the Grey Image to 550*550 (Make sure your face is in the center in all image)
 			Final_Image = np.array(Crop_Image, "uint8")
 			#print(Numpy_Image)
-			faces = face_cascade.detectMultiScale(Final_Image, scaleFactor=1.5, minNeighbors=5) #Detect The face in all sample image 
+			faces = face_cascade.detectMultiScale(Final_Image, scaleFactor=1.5, minNeighbors=5) #used to find the faces ; 
+			#the scaleFactor parameter used to specify how much the image size is redused at each image scale
+			#minNeighbors parameter used to specify how many neighbors each candidate rectangle should have to retain it.
 			print (Face_ID,faces)
 
 			for (x,y,w,h) in faces:
-				roi = Final_Image[y:y+h, x:x+w] #crop the Region of Interest (ROI)
+				roi = Final_Image[y:y+h, x:x+w] #crop the Region of Interest (ROI), it allows you to select a rectangle from the upper left corner to the lower right point.		
 				x_train.append(roi)
 				y_ID.append(Face_ID)
 
